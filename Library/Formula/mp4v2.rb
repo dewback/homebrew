55867dd2d308a9dfa764c1168f33170829085346
Fabián Arias <fabian@fabianarias.com>
2011-07-07 22:18:48 -0400

    Added mp4v2 head

--- Library/Formula/mp4v2.rb
+++ Library/Formula/mp4v2.rb
@@ -5,7 +5,10 @@ class Mp4v2 < Formula
   homepage 'http://code.google.com/p/mp4v2/'
   md5 '986701929ef15b03155ac4fb16444797'
 
+  head 'http://mp4v2.googlecode.com/svn/trunk/'
+
   def install
+    system "autoreconf -fi" if ARGV.build_head?
     system "./configure", "--disable-debug", "--disable-dependency-tracking", "--prefix=#{prefix}"
     system "make install"
   end
